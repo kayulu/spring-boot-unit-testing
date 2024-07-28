cf4f1d0f9847b69cad6d08e7833f3b0e19a760ec
Kayhan <Kayhan@ulu.de>
2024-07-28 12:39:44 +0200

    Add dependency for Spring Boot test support, and test directory

---
+++ 2.00-starting-project/src/test/java/com/kayulu/test/ApplicationExampleTest.java
@@ -0,0 +1,13 @@
+package com.kayulu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationExampleTest {
+
+    @Test
+    void basicTest() {
+
+    }
+}
